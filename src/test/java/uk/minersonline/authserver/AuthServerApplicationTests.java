3121d7d3f64824357a34f1814814240d9abbd8ef
ajh123 <ajh123@users.noreply.github.com>
2024-03-09 13:25:35 +0000

    feat: begin spring app

---
+++ src/test/java/uk/minersonline/authserver/AuthServerApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.minersonline.authserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
