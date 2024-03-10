475ccf82569658d50a035cb14c87c46ce37b7060
ajh123 <ajh123@users.noreply.github.com>
2024-03-10 20:12:19 +0000

    fix: add router as handler + use simpler jwt

--- authserver/src/main/java/uk/minersonline/auth/server/MainVerticle.java
+++ authserver/src/main/java/uk/minersonline/auth/server/MainVerticle.java
@@ -4,6 +4,7 @@ import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Promise;
 import io.vertx.core.http.HttpServer;
 import io.vertx.ext.auth.KeyStoreOptions;
+import io.vertx.ext.auth.PubSecKeyOptions;
 import io.vertx.ext.auth.jwt.JWTAuth;
 import io.vertx.ext.auth.jwt.JWTAuthOptions;
 import io.vertx.ext.web.Router;
@@ -26,11 +27,10 @@ public class MainVerticle extends AbstractVerticle {
     List<HTTPService> responseBuilders = pluginManager.getExtensions(HTTPService.class);
 
     // create jwt provider
-    JWTAuthOptions config = new JWTAuthOptions()
-      .setKeyStore(new KeyStoreOptions()
-        .setPath("keystore.jceks")
-        .setPassword("secret"));
-    JWTAuth provider = JWTAuth.create(vertx, config);
+    JWTAuth provider = JWTAuth.create(vertx, new JWTAuthOptions()
+      .addPubSecKey(new PubSecKeyOptions()
+        .setAlgorithm("HS256")
+        .setBuffer("keyboard cat")));
 
     Users users = new Users(provider);
 
@@ -39,6 +39,7 @@ public class MainVerticle extends AbstractVerticle {
       HttpServer server = vertx.createHttpServer();
       Router router = Router.router(vertx);
       builder.getHandler(router, users);
+      server.requestHandler(router);
       server.listen(builder.getPort(), http -> {
         if (http.succeeded()) {
           startCount.incrementAndGet();
