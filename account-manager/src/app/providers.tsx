445575bbf10da4a56bfddbb77b1c2d02f59cd6c2
Samuel Hulme <41990982+ajh123@users.noreply.github.com>
2025-02-05 20:01:23 +0000

    feat(account-manager): implement main layout and home page with Carbon components

---
+++ account-manager/src/app/providers.tsx
@@ -0,0 +1,21 @@
+'use client';
+
+import React from 'react';
+import MainHeader from '@/components/MainHeader/MainHeader';
+import { Content, Theme } from '@carbon/react';
+
+
+interface Props {
+    children: React.ReactNode
+}
+
+export function Providers({ children }: Props) {
+    return (
+        <div>
+            <Theme theme="g100">
+                <MainHeader />
+            </Theme>
+            <Content>{children}</Content>
+        </div>
+    );
+}
