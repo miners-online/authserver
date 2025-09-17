39fa412e5fce5fe0a280cccb6f5b4bffc1598ff5
Samuel Hulme <41990982+ajh123@users.noreply.github.com>
2025-09-17 13:26:07 +0100

    feat: add initial better auth setup

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,15 @@
 import { Hono } from 'hono'
+import { D1Database } from '@cloudflare/workers-types'
+import { auth, initAuth } from './lib/auth';
 
-const app = new Hono()
+type Bindings = {
+  DB: D1Database;
+};
+
+const app = new Hono<{ Bindings: Bindings }>()
 
 app.get('/', (c) => {
+  initAuth(c.env);
   return c.text('Hello Hono!')
 })
 
